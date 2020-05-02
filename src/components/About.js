0d5ffad3a931b7aa3d07b4ce090af75fba7e3d2f
Nima Mohajeri <nima.mohajeri0@gmail.com>
2020-05-02 23:25:10 +0300

    app not loading properly on gh-pages site

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,8 @@ const About = () => {
 
         <div className="container">
             <h4 className="center">About </h4>
-            <p>Hi, My name is Nima Mohajeri. This is my first React app for practice</p>
+            <p>Hi, My name is Nima Mohajeri!</p>
+            <p>This is my first React app for practice.</p>
             
 
            
